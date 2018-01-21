0911d2c36f819fe9211bac4c8f576ac7579aa950
Minjae Son <bungabear6422@gmail.com>
2018-01-21 21:45:46 +0900

    RetrofitClass.java 재등록

---
+++ app/src/main/java/com/bungabear/inubus/RetrofitClass.java
@@ -0,0 +1,33 @@
+package com.bungabear.inubus;
+
+import retrofit2.Retrofit;
+import retrofit2.Retrofit.Builder;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class RetrofitClass
+{
+    private static final boolean DEBUG = false;
+    private static final String TAG = "Retrofit Debug";
+    private static RetrofitClass instance;
+    private static Retrofit retrofit = null;
+    private static RetrofitService retrofitService = null;
+
+    private RetrofitClass()
+    {
+        retrofit = new Retrofit
+                .Builder()
+                .baseUrl(KeyClass.serverAddr)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        retrofitService = (RetrofitService)retrofit.create(RetrofitService.class);
+    }
+
+    public static RetrofitService getInstance()
+    {
+        if (instance == null) {
+            instance = new RetrofitClass();
+        }
+        RetrofitClass localRetrofitClass = instance;
+        return retrofitService;
+    }
+}
